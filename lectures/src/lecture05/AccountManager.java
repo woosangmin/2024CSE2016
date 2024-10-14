349184b61d7e527548cc59ff0dc64e7fc88bb1a0
Sangmin Woo <woosangmin@gmail.com>
2024-10-14 14:26:12 +0900

    * add lecture05

---
+++ lectures/src/lecture05/AccountManager.java
@@ -0,0 +1,17 @@
+/** 시동 클래스
+ * 입력 포맷
+ *     D 금액 - 입금
+ *     W 금액 - 출금
+ *     Q     - 종료
+ * 출력
+ *    거래 결과 */
+public class AccountManager {
+
+    public static void main(String[] args) {
+        BankReader reader = new BankReader();
+        BankAccount account = new BankAccount(0);
+        BankWriter writer = new BankWriter("한양은행", account);
+        AccountController controller = new AccountController(reader, writer, account);
+        controller.processTransaction();
+    }
+}
